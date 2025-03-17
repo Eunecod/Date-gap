18d1c00d75adfae18e09cbd2a6da54bce0807d66
Eugene <sudakovzhenya02@gmail.com>
2025-03-17 21:10:52 +0700

    [feat] Новый формат даты
    - При вводе даты раставляются разделители "."

--- app/src/main/java/com/example/date_gap/MainActivity.kt
+++ app/src/main/java/com/example/date_gap/MainActivity.kt
@@ -9,6 +9,7 @@ import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.widget.doOnTextChanged
 
 data class Date(val day: Int, val month: Int, val year: Int)
 
@@ -26,22 +27,54 @@ class MainActivity : ComponentActivity() {
             val between = calculationDateGap(startDate, endDate)
             findViewById<TextView>(R.id.diff_day).text = between.toString()
         }
+
+        findViewById<EditText>(R.id.edit_start_date).doOnTextChanged { text, start, before, count ->
+            dateEditFormat(findViewById<EditText>(R.id.edit_start_date), start, before, count)
+        }
+
+        findViewById<EditText>(R.id.edit_end_date).doOnTextChanged { text, start, before, count ->
+            dateEditFormat(findViewById<EditText>(R.id.edit_end_date), start, before, count)
+        }
     }
 
     @SuppressLint("NewApi")
     fun calculationDateGap(start: Date?, end: Date?): Long {
-        if (start != null && end != null) {
+        if (start != null && end != null && start != end) {
             return ChronoUnit.DAYS.between(LocalDate.of(start.year, start.month, start.day), LocalDate.of(end.year, end.month, end.day)) + 1
         }
 
-        return 0;
+        return 0
     }
 
     private fun dateParse(dateStr: String): Date? {
-        if(dateStr.length != 8) {
+        if(dateStr.length != 10) {
             return null
         }
 
-        return Date(day=dateStr.substring(0, 2).toInt(), month=dateStr.substring(2, 4).toInt(), year=dateStr.substring(4, 8).toInt());
+        return Date(day=dateStr.substring(0, 2).toInt(), month=dateStr.substring(3, 5).toInt(), year=dateStr.substring(6, 10).toInt())
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun dateEditFormat(dateText: EditText, start: Int, before: Int, count: Int) {
+        val rawStr = dateText.text.toString()
+        dateText.setSelection(rawStr.length)
+
+        if (rawStr.length > 10) {
+            dateText.setText(rawStr.substring(0, 10))
+            return
+        }
+
+        if (rawStr.length == 2 && before == 0 && count == 1) {
+            dateText.setText(rawStr.substring(0, 2) + ".")
+        }
+
+        if (rawStr.length == 5 && before == 0 && count == 1) {
+            dateText.setText(rawStr.substring(0, 5) + ".")
+        }
+
+        if (before == 1 && count == 0 && (start == 2 || start == 5)) {
+            dateText.setText(rawStr.substring(0, start - 1) + rawStr.substring(start))
+            dateText.setSelection(start - 1)
+        }
     }
 }
