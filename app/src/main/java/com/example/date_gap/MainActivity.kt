bbd12aaf5a7e519c693b031ec70527eb0aca0405
Eugene <sudakovzhenya02@gmail.com>
2025-03-22 00:09:00 +0700

    [refactor] Поддержка нового кода
    - Поддержал изменение и разбивку на фращменты логику и интерфейса

--- app/src/main/java/com/example/date_gap/MainActivity.kt
+++ app/src/main/java/com/example/date_gap/MainActivity.kt
@@ -7,33 +7,57 @@ import android.widget.EditText
 import android.widget.TextView
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
-import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.date_gap.service.DateHelper
+import com.example.date_gap.service.Date
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-data class Date(val day: Int, val month: Int, val year: Int)
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
+    private lateinit var adapter: ViewPageAdapter
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+    private var dateHelper: DateHelper = DateHelper()
+    private val tabNames: Array<String> = arrayOf(
+        "Кол-во дней",
+        "Расчёт даты"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_main)
 
+        adapter = ViewPageAdapter(this)
+        viewPager = findViewById<ViewPager2>(R.id.view_pager)
+        viewPager.adapter = adapter
+
+        tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+           tab.text = tabNames[position]
+        }.attach()
+
         findViewById<Button>(R.id.btn_calculate).setOnClickListener {
-            val startDate = dateParse(findViewById<EditText>(R.id.edit_start_date).text.toString())
-            val endDate = dateParse(findViewById<EditText>(R.id.edit_end_date).text.toString())
+            if(tabLayout.selectedTabPosition == 0) {
+                val startDate = dateHelper.parse(findViewById<EditText>(R.id.edit_start_date).text.toString())
+                val endDate = dateHelper.parse(findViewById<EditText>(R.id.edit_end_date).text.toString())
 
-            val between = calculationDateGap(startDate, endDate)
-            findViewById<TextView>(R.id.diff_day).text = between.toString()
-        }
+                val between = calculationDateGap(startDate, endDate)
+                findViewById<TextView>(R.id.diff_day).text = between.toString()
+            }
+            else if(tabLayout.selectedTabPosition == 1) {
+                val baseDate = dateHelper.parse(findViewById<EditText>(R.id.edit_base_date).text.toString())
+                val countDay = findViewById<EditText>(R.id.edit_count_day).text.toString()
 
-        findViewById<EditText>(R.id.edit_start_date).doOnTextChanged { text, start, before, count ->
-            dateEditFormat(findViewById<EditText>(R.id.edit_start_date), start, before, count)
-        }
-
-        findViewById<EditText>(R.id.edit_end_date).doOnTextChanged { text, start, before, count ->
-            dateEditFormat(findViewById<EditText>(R.id.edit_end_date), start, before, count)
+                if(baseDate != null && countDay != "") {
+                    findViewById<TextView>(R.id.calc_date).text = dateHelper.addDays(baseDate, countDay.toInt())
+                }
+            }
         }
     }
 
@@ -44,37 +68,5 @@ class MainActivity : ComponentActivity() {
         }
 
         return 0
-    }
-
-    private fun dateParse(dateStr: String): Date? {
-        if(dateStr.length != 10) {
-            return null
-        }
-
-        return Date(day=dateStr.substring(0, 2).toInt(), month=dateStr.substring(3, 5).toInt(), year=dateStr.substring(6, 10).toInt())
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun dateEditFormat(dateText: EditText, start: Int, before: Int, count: Int) {
-        val rawStr = dateText.text.toString()
-        dateText.setSelection(rawStr.length)
-
-        if (rawStr.length > 10) {
-            dateText.setText(rawStr.substring(0, 10))
-            return
-        }
-
-        if (rawStr.length == 2 && before == 0 && count == 1) {
-            dateText.setText(rawStr.substring(0, 2) + ".")
-        }
-
-        if (rawStr.length == 5 && before == 0 && count == 1) {
-            dateText.setText(rawStr.substring(0, 5) + ".")
-        }
-
-        if (before == 1 && count == 0 && (start == 2 || start == 5)) {
-            dateText.setText(rawStr.substring(0, start - 1) + rawStr.substring(start))
-            dateText.setSelection(start - 1)
-        }
     }
 }
